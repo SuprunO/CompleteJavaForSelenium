915c9f1bc43df10949fb476bc35e249af339c8b6
SuprunO <oleksiisuprun@gmail.com>
2017-01-13 11:54:42 +0200

    13.01.2016 Objects

---
+++ src/test/java/Objects.java
@@ -0,0 +1,44 @@
+/**
+ * Created by alex on 13.01.2017.
+ */
+public class Objects {
+
+        String str2 = "Hi There !!!";
+
+        /***
+         * [][][][CarClass - c1][CarClass - c2][][][][][]
+         * @param args
+         */
+
+        public static void main(String[] args) {
+            String str1 = "Hello Java";
+
+            System.out.println(str1);
+
+//		for (int i=0; i < args.length; i++) {
+//			System.out.println(args[i]);
+//		}
+
+            System.out.println("*************************************");
+
+            CarClass c1 = new CarClass();
+            CarClass c2 = new CarClass();
+
+//		c1.model = "BMW";
+//		c1.year = 2015;
+
+            System.out.println(c1.year);
+            System.out.println(c1.getYear());
+        }
+    }
+
+    class CarClass {
+
+        int year = 2000;
+        String model;
+
+        public int getYear() {
+            return this.year;
+        }
+    }
+

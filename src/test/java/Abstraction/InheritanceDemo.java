40ad522d9a895d1b986a09eaba248b3db62432f6
SuprunO <oleksiisuprun@gmail.com>
2017-01-16 21:18:16 +0200

    16.01.2016 Commit Interfaces +Abstractions

---
+++ src/test/java/Abstraction/InheritanceDemo.java
@@ -0,0 +1,23 @@
+package Abstraction;
+
+import Abstraction.automobile.BMW;
+import Abstraction.automobile.Car;
+
+/**
+ * Created by alex on 16.01.2017.
+ */
+public class InheritanceDemo {
+    public static void main(String[] args) {
+    int speed =0;
+
+        Car c1 = new Car(speed);
+        c1.increaseSpeed();
+
+        BMW bmw1=new BMW(speed);
+        bmw1.increaseSpeed();
+        bmw1.displayClassNavigation();
+
+
+    }
+
+}
